915a139dd5aefed3dfef6741d94636513ead7bcd
JoseCondor <jose.condor@vallegrande.edu.pe>
2025-09-02 20:49:10 +0000

    dbAgro

---
+++ T07_filegroups_partitions.sql
@@ -0,0 +1,8 @@
+-- =====================================================================================
+-- AgroConecta - Script de Filegroups y Particionamiento
+-- Autor: Team 07
+-- Descripción: Este script implementa una estrategia de particionamiento de datos
+--              en las tablas 'Pedidos' y 'Detalle_Pedido' para optimizar el
+--              rendimiento y la gestión de datos históricos.
+-- =====================================================================================
+
---
+++ T07_tables_temp.sql
@@ -0,0 +1,5 @@
+-- Script de tablas temporales para el análisis de ventas en AgroConecta
+-- Autor: Team 07
+-- Descripción: Este script crea tablas temporales para generar reportes y análisis
+-- sobre los datos de ventas y productos.
+
